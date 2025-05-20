b9e26684e1c94ec478f71c8917da7ab05aef9c8e
Caots <honghoang1232@gmail.com>
2025-05-21 01:01:23 +0700

    update package

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,14 @@
 import LoadingScreen from '@/components/LoadingScreem';
 import { SCREEN_KEY } from '@/constants/common';
 import { useAuthContext } from '@/providers/UserProvider';
-import { Redirect, useRootNavigationState } from 'expo-router';
+import { Redirect } from 'expo-router';
 import React from 'react';
 import 'react-native-reanimated';
 
 export default function Page() {
-    const rootNavigationState = useRootNavigationState();
+    // const rootNavigationState = useRootNavigationState();
     const { loading, session } = useAuthContext();
-    if (!rootNavigationState?.key) return null;
+    // if (!rootNavigationState?.key) return null;
 
     if (!loading && session) {
         return <Redirect href={SCREEN_KEY.home} />;
